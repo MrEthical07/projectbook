1c5259da911238666e6fa7c2f39c453fe91bab7a
MrEthical07 <ayushgaikwad06@gmail.com>
2026-02-19 01:00:56 +0530

    duplicate page removed

--- src/routes/(home)/add-project/+page.ts
+++
@@ -1,9 +0,0 @@
-import { getAddProjectReference, getWorkspaceDashboard } from "$lib/remote/workspace.remote";
-
-export async function load() {
-	const dashboard = await getWorkspaceDashboard();
-	return {
-		reference: await getAddProjectReference(),
-		userId: dashboard.user.id
-	};
-}
